4dec37fd28b12350a9d2dd071078d57f3fdbf42a
Yash Suthar <yashrsuthar90@gmail.com>
2025-01-14 22:18:21 +0530

    update files

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+// Hamburger menu toggle
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.toggle('show');
+});
